c150eba3e96c3b3494710229614a84cedd31ac16
Ilya Smelkov <triplepointfive@gmail.com>
2015-10-11 23:29:03 +0300

    Church numbers

---
+++ src/ChurchNumbers.hs
@@ -0,0 +1,19 @@
+module ChurchNumbers where
+
+class Number a where
+  c0, c1, c2, c3 :: (a -> a) -> a -> a
+  c0 = \ s -> \ z -> z
+  c1 = \ s -> \ z -> s z
+  c2 = \ s -> \ z -> s (s z)
+  c3 = \ s -> \ z -> s (s (s z))
+
+  scc :: ((a -> a) -> a -> a) -> ((a -> a) -> a -> a)
+  scc = \ n -> \ s -> \z -> n s (s z)
+
+  plus :: ((a -> a) -> a -> a) -> ((a -> a) -> a -> a) -> (a -> a) -> a -> a
+  plus = \ n -> \ m -> \ s -> \ z -> m s (n s z)
+
+  times :: ((a -> a) -> a -> a) -> ((a -> a) -> a -> a) -> (a -> a) -> a -> a
+  times = \ m -> \ n -> \ s -> \ z -> m (n s) z
+
+instance Number Int
