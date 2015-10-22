0b31256e5eedc24ffc263added052a0cb46c7290
Ilya Smelkov <triplepointfive@gmail.com>
2015-10-22 07:53:46 +0300

    Simple bool types

--- src/SimpleBool.hs
+++ src/SimpleBool.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE DeriveAnyClass  #-}
 {-# LANGUAGE DeriveGeneric   #-}
-module LambdaCalculus where
+module SimpleBool where
 
 import Control.Exception (catch, throw, Exception, evaluate)
 import Data.Typeable (Typeable)
@@ -15,7 +15,7 @@ data Info = Info
 data Ty
     = TyBool
     | TyArr Ty Ty
-    deriving (Show, NFData, Generic)
+    deriving (Show, NFData, Generic, Eq)
 
 data Binding
     = NameBinding
@@ -47,3 +47,36 @@ index2name _ ctx i = fst (ctx !! i)
 
 getBinding :: Info -> Context -> Int -> Binding
 getBinding _ ctx i = snd (ctx !! i)
+
+typeOf :: Context -> Term -> Ty
+typeOf _ (TmTrue _)             = TyBool
+typeOf _ (TmFalse _)            = TyBool
+typeOf ctx (TmIf fi t1 t2 t3)   = typeOfIf ctx fi t1 t2 t3
+typeOf ctx (TmVar fi i _)       = getTypeFromContext fi ctx i
+typeOf ctx (TmAbs fi x tyT1 t2) = typeOfAbs fi ctx x tyT1 t2
+typeOf ctx (TmApp fi t1 t2)     = typeOfApp fi ctx t1 t2
+
+typeOfIf :: Context -> Info -> Term -> Term -> Term -> Ty
+typeOfIf ctx fi t1 t2 t3
+    | (/=) (typeOf ctx t1) TyBool = error $
+        show fi ++ "guard of conditional not a boolean"
+    | (/=) tyT2 (typeOf ctx t3)   = error $
+        show fi ++ "arms of conditional have different types"
+    | otherwise = tyT2
+  where
+    tyT2 = typeOf ctx t2
+
+typeOfAbs :: Info -> Context -> String -> Ty -> Term -> Ty
+typeOfAbs fi ctx x tyT1 t2 = TyArr tyT1 tyT2
+  where
+    ctx' = addBinding ctx x (VarBind tyT1)
+    tyT2 = typeOf ctx' t2
+
+typeOfApp :: Info -> Context -> Term -> Term -> Ty
+typeOfApp fi ctx t1 t2 = case tyT1 of
+    TyArr tyT11 tyT12 -> if (==) tyT2 tyT11 then tyT12
+        else error $ show fi ++ "parameter type mismatch"
+    _ -> error $ show fi ++ "arrow type expected"
+  where
+    tyT1 = typeOf ctx t1
+    tyT2 = typeOf ctx t2
